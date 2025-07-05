7928610f4f1e34ae53934090e86fc9b880e9788f
Uttpal Singh <uttpalsingh8@gmail.com>
2025-07-05 11:38:06 +0000

    datatypes

---
+++ lecture2/script.js
@@ -0,0 +1,28 @@
+// Data types
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true and false
+null => standalone value
+undefined => value is not assign
+symbol => unique
+*/
+
+// object
+
+let a = 2 // number data types
+let b = 3
+let c = 'five' // string data types
+
+let sum = a + b + c
+
+console.log(sum)
+console.log(typeof a) // number
+console.log(typeof c) // string
+console.log( typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
