db06f380602caef7ed1fa71bb7f79ca377f5b467
Uttpal Singh <uttpalsingh8@gmail.com>
2025-07-07 09:55:50 +0000

    datatypes conversion and operations

--- lecture3/script.js
+++ lecture3/script.js
@@ -32,3 +32,56 @@ let isBoolean = Boolean(bool)
 // 0 => false
 // " " => false
 // "vigi" => true
+
+// ******************** Operations ******************
+
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); => 12
+// console.log(+""); => 0
+// console.log(+true); => 1
+// console.log(true+); => error
+
+
+
+
+
+
+// Postfix increment (suffix increment)
+
+let X = 3;
+const Y = x++;
+// console.log(y);
+// console.log(x);
+
+// x is 4; y is 3
+
+let X2 = 3n;
+const Y2 = x2++;
+// console.log(x2);
+// console.log(y2);
+
+// x2 is 4n; y2 is 3n
+
+
+// Prefix increment
+
+let x = 3;
+const y = ++x;
+console.log(x);
+console.log(y);
+
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(x2);
+console.log(y2);
+
+// x2 is 4n; y2 is 4n
+
+// For more detail visit this page
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// ++(++x); // SyntaxError: Invalid left-hand side expression in prefix operation
