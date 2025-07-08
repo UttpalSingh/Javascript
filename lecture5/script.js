b31da195ae2a4134eb9991dd8e305ac8f34264c4
Uttpal Singh <uttpalsingh8@gmail.com>
2025-07-08 15:29:13 +0000

    strings

---
+++ lecture5/script.js
@@ -0,0 +1,201 @@
+// strings
+
+let name = "vigi"
+let repoCount = 5
+
+
+// `` => backtics
+
+// console.log(`Hlw my name is ${name} and my repo count is ${repoCount}`)
+
+const newName= new String('vigi') 
+// console.log(newName[0]);
+// console.log(newName[3]);
+// console.log(newName.length);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt());
+// console.log(newName.charAt(2));
+
+const oldName = "      Tonystark  "
+// console.log(oldName); //      tonystark
+// console.log(oldName.trim()); //Tonystark  trim function remove the extra space
+
+const userData = "Shaktiman"
+// console.log(userData.substring(0,5));
+// console.log(userData.slice(0,5));
+
+const Var = "No i am good"
+// console.log(Var.replace('No','Yes'));
+
+const secondVar = "he is a developer"
+console.log(secondVar.split('-'));
+
+const str1 = "Hello";
+const str2 = "World";
+
+console.log(str1.concat(" ", str2));
+// Expected output: "Hello World"
+
+console.log(str2.concat(", ", str1));
+// Expected output: "World, Hello"
+
+// methods in javascript
+/*
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ 
+*/
+
