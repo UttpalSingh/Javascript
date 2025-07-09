06de84df70569c00804c4cc1dad06d43c85d3e10
Uttpal Singh <uttpalsingh8@gmail.com>
2025-07-09 17:39:09 +0000

    Date and Time

---
+++ lecture7/script.js
@@ -0,0 +1,26 @@
+// Date in Js
+
+const myDate = new Date() // Object
+// console.log(myDate.toString()); // Wed Jul 09 2025 17:09:53 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Wed Jul 09 2025
+// console.log(myDate.toLocaleString()); // 7/9/2025, 5:09:53 PM
+// console.log(myDate.toLocaleDateString()); // 7/9/2025
+
+// const newDate = new Date("01-12-2025") // Month:Date:year
+const newDate = new Date(2025,0,31) // Year:Month:Date
+// console.log(newDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(newDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let mydate = new Date()
+// console.log(mydate.toLocaleString());
+// console.log(mydate.getDay());
+// console.log(mydate.getHours());
+// console.log(mydate.getMonth());
+// console.log(mydate.getTime());
+
+
+
