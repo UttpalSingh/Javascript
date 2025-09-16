58e70346094d9cb0550839abb26836e1ba9cf701
Uttpal Singh <uttpalsingh8@gmail.com>
2025-09-16 08:51:35 +0000

    Promise in js

---
+++ lecture21/script.js
@@ -0,0 +1,96 @@
+    /*
+    resove connection is with .then
+    reject connectio is with .catch
+    
+    */
+    
+    // first promise
+    
+    // const promise = new Promise((resolve,reject)=>{
+    //     // Do an async task
+    //     setTimeout(()=>{
+    //         console.log("async task is complete");
+    //         resolve()
+            
+    //     },1000)
+    // })
+
+    // promise.then((e)=>{
+    //     console.log("promised consumed");
+        
+    // })  
+
+
+    // // second promise
+    // new Promise((resolve,reject)=>{
+    //     setTimeout(()=>{
+    //         console.log("asycs task 2");
+    //         resolve()
+    //     },1000)
+    // }).then((e)=>{
+    //     console.log("promise 2 consumed");
+  
+    // })
+
+    // // third promise
+    // const newPromise = new Promise((resolve,reject)=>{
+    //     setTimeout(()=>{
+    //         resolve({username : "VIGI",emai: "vigi@gmail.com"})
+    //     },1000)
+    // })
+    // newPromise.then((e)=>{
+    //     console.log(e)
+    // })
+
+
+    // // fourth promise
+    // const promiseFour = new Promise((resolve,reject)=>{
+    //     setTimeout(() => {
+    //         let error = true
+    //         if(!error){
+    //             resolve({username:"Uttpal",Password:"123uttpal"})
+    //         }else{
+    //             reject('ERROR : something goes wrong')
+    //         }
+    //     }, 1000);
+    // })
+    // promiseFour
+    // .then((e)=>{
+    //     console.log(e);
+    //     return e.username
+    // })
+    // .then((e)=>{
+    //     console.log(e);
+        
+    // })
+    // .catch((error)=>{
+    //     console.log(error);
+        
+    // }).finally(()=>{
+    //     console.log("Promise if either resolve or reject");
+        
+    // })
+
+
+    // promise five (async await)
+    const promiseFive = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            let error = true
+            if(!error){
+                resolve({username : "Javascript",password : "123js"})
+            }
+            else{
+                reject('ERROR : there is something wrong in your code')
+            }
+        }, 1000);
+    })
+    async function consumePromiseFive() {
+        try {
+            const response =  await promiseFive
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    consumePromiseFive()
